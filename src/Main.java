6f12b7e4c5f2303e450852240e4ad6bcc4047f59
Zein <zeinmusical@gmail.com>
2026-05-29 17:31:55 +0300

    test #4

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,6 @@ import DataStructures.*;
 
 public class Main {
     public static void main(String[] args) {
-        Heap h = new Heap(false);
-        h.insert(10);
-        h.insert(-10);
-        h.insert(3);
-        h.insert(100);
-        while(!h.isEmpty()) {
-            System.out.println(h.pop());
-        }
+        
     }
 }
