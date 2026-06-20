657634c0df450bab064fb37efd28152cf0bdb21d
Zein <zeinmusical@gmail.com>
2026-06-20 15:11:23 +0300

    Finished the Graph class with all of its functions and other additional classes

--- src/Problem2/Main.java
+++ src/Problem2/Main.java
@@ -1,5 +1,7 @@
-package Problem-2;
+package Problem2;
 
 public class Main {
-
+    public static void main(String[] args) {
+        
+    }
 }
---
+++ src/Problem2/Pair.java
@@ -0,0 +1,11 @@
+package Problem2;
+
+public class Pair {
+    int a;
+    int b;
+
+    public Pair(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+}
