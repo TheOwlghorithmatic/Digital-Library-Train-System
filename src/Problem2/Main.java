621cd9ddc33413272b73035ab3d3b099da22c5e0
Zein <zeinmusical@gmail.com>
2026-06-20 15:40:59 +0300

    finishing uppp

--- src/Problem2/Main.java
+++ src/Problem2/Main.java
@@ -1,17 +1,7 @@
 package Problem2;
 
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        Graph graph = new Graph();
-        int m = sc.nextInt();
-        for(int i=0;i<m;i++) {
-            int u = sc.nextInt(), v = sc.nextInt(), w = sc.nextInt();
-            graph.add(u, v, w);
-        }
-        sc.close();
-        System.out.println(graph.shortestPathBetween(0,4));
+        
     }
 }
